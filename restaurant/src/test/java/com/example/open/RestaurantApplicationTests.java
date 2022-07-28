53fe516622dcf5051f9a07d7af4ecb1bde1367fc
ddudukda <dk.kwon094@gmail.com>
2022-07-28 16:00:03 +0900

    naver open api - 검색 프로젝트 생성 및 지역검색, 이미지 검색 url 연결 테스트 확인

---
+++ restaurant/src/test/java/com/example/open/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.open;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ restaurant/src/test/java/com/example/open/wishlist/repository/WishListRepositoryTest.java
@@ -0,0 +1,92 @@
+package com.example.open.wishlist.repository;
+
+import com.example.open.wishlist.entity.WishListEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@DisplayName("레파지토리 테스트")
+@SpringBootTest
+class WishListRepositoryTest {
+
+    @Autowired private WishListRepository wishListRepository;
+    
+    private WishListEntity create(){
+        var wishList = new WishListEntity();
+        wishList.setTitle("title");
+        wishList.setCategory("category");
+        wishList.setAddress("address");
+        wishList.setRoadAdress("road-adress");
+        wishList.setHomePageLink("");
+        wishList.setImageLink("");
+        wishList.setVisit(false);
+        wishList.setVisitCount(0);
+        wishList.setLastVisitDate(null);
+        
+        return wishList;
+    }
+    
+    @DisplayName("save 테스트")
+    @Test
+    public void saveTest(){
+        var wishList = create();
+        var expected = wishListRepository.save(wishList);
+
+        Assertions.assertNotNull(expected);
+        Assertions.assertEquals(1, expected.getIndex());
+
+    }
+
+    @DisplayName("update 테스트")
+    @Test
+    public void updateTest(){
+        var wishListEntity = create();
+        var expected = wishListRepository.save(wishListEntity);
+
+        expected.setTitle("update test");
+        var saveEntity = wishListRepository.save(expected);
+
+        Assertions.assertEquals("update test", saveEntity.getTitle());
+        Assertions.assertEquals(1, wishListRepository.listAll().size());
+    }
+
+    @DisplayName("find 테스트")
+    @Test
+    public void findByIdTest(){
+        var wishList = create();
+        wishListRepository.save(wishList);
+
+        var expected = wishListRepository.findById(1);
+
+        Assertions.assertEquals(true, expected.isPresent());
+        Assertions.assertEquals(1, expected.get().getIndex());
+    }
+
+    @DisplayName("delete 테스트")
+    @Test
+    public void deleteTest(){
+        var wishList = create();
+        wishListRepository.save(wishList);
+
+        wishListRepository.deleteById(1);
+
+        int count = wishListRepository.listAll().size();
+        Assertions.assertEquals(0, count);
+    }
+
+    @DisplayName("list 테스트")
+    @Test
+    public void ListAllTest(){
+        var wishListEntity1 = create();
+        wishListRepository.save(wishListEntity1);
+
+        var wishListEntity2 = create();
+        wishListRepository.save(wishListEntity2);
+
+        int count = wishListRepository.listAll().size();
+
+        Assertions.assertEquals(2, count);
+    }
+}
